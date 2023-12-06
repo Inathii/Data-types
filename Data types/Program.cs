de206bf2d4869c1c06ecfdf5692c5ff6f824034f
A252426 <A252426@LSBSA-BC86HW3>
2023-12-06 06:28:26 +0200

    Add project files.

---
+++ Data types/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declaration
+            string name = "John";
+            int age = 35;
+            bool isTall = false;
+            double weight = 80.2;
+            Console.WriteLine(name + " is " + age + " he weighs " + weight);
+            Console.WriteLine("It is  " + isTall + " that he is tall. ");
+
+
+            //freeze console
+            Console.ReadLine();
+
+        }
+    }
+}
